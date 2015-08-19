784948361c59ac56a1dcd1a92a8752ac5e858af3
David Ilizarov <dilizarov@gmail.com>
2015-08-19 04:45:46 -0500

    Made a functioning transition between data in one tableview on the ProfileViewController.

--- Community/ProfileViewController.swift
+++ Community/ProfileViewController.swift
@@ -11,13 +11,40 @@ import UIKit
 class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     var communities = [NSString]()
+    var communities2 = [NSString]()
+    var notifs = false
+    
     @IBOutlet var communitiesTable: UITableView!
+    
+    
+    @IBOutlet var leftButton: UIButton!
+    @IBAction func leftButtonPressed(sender: AnyObject) {
+        notifs = false
+        communitiesTable.setContentOffset(CGPointZero, animated: false)
+        communitiesTable.reloadData()
+    }
+    
+    @IBOutlet var notifications: UIButton!
+    @IBAction func notificationsButtonPressed(sender: AnyObject) {
+        notifs = true
+        communitiesTable.setContentOffset(CGPointZero, animated: false)
+
+        communitiesTable.reloadData()
+    }
+    
+    @IBOutlet var settings: UIButton!
+    @IBAction func settingsPressed(sender: AnyObject) {
+        
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     
-        communities = ["Hi", "This is community", "This is another"]
+        communities = ["Hi", "This is community", "This is another", "omg", "why", "what", "how", "this", "is", "real", "life", "run", "popp"]
+        
+        communities2 = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u"]
         
         communitiesTable.tableHeaderView = UIView(frame: CGRectMake(0, 0, 0, 30))
         communitiesTable.tableFooterView = UIView(frame: CGRectZero)
@@ -27,14 +54,25 @@ class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewD
     
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return communities.count
+        if (notifs) {
+            return communities2.count
+        } else {
+            return communities.count
+        }
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         var cell = self.communitiesTable.dequeueReusableCellWithIdentifier("communityCell") as! CommunityCell
         
-        if (self.communities.count > indexPath.row) {
-             cell.configureViews(communities[indexPath.row])
+        if (notifs) {
+            if (self.communities2.count > indexPath.row) {
+                cell.configureViews(communities2[indexPath.row])
+            }
+
+        } else {
+            if (self.communities.count > indexPath.row) {
+                cell.configureViews(communities[indexPath.row])
+            }
         }
         
         return cell
